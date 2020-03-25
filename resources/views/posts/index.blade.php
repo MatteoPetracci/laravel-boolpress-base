e15cb813923225b5d530cd8164647a347f472030
Matteo <matteop18@gmail.com>
2020-03-25 16:44:06 +0100

    stampo a schermo tutti i post in index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.layout')
 @section('main')
-    @dd($allPosts);
+    {{-- @dd($allPosts); --}}
+    <h2>All Cameras</h2>
+    @foreach ($allPosts as $post)
+        <ul style="list-style:none">
+            <li>{{$post->id}}</li>
+            <li>{{$post->image}}</li>
+            <li>{{$post->title}}</li>
+            <li>{{$post->description}}</li>
+            <li>{{$post->created_at}}</li>
+            <li>{{$post->updated_at}}</li>
+        </ul>
+    @endforeach
 @endsection
