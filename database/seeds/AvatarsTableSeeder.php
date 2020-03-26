944ec17faeec1352d82f7d3a1e3feef2820448cb
Matteo <matteop18@gmail.com>
2020-03-26 17:50:53 +0100

    inizio seeder avatars

---
+++ database/seeds/AvatarsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Avatar;
+use Faker\Generator as Faker;
+
+class AvatarsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //
+    }
+}
