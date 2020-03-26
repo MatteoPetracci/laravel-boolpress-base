55c448ac2dd000a04a4f9a54a2c937d26334c84a
Matteo <matteop18@gmail.com>
2020-03-26 15:41:52 +0100

    migration

--- database/migrations/2020_03_26_143655_create_photos_table.php
+++ database/migrations/2020_03_26_143655_create_photos_table.php
@@ -17,7 +17,7 @@ class CreatePhotosTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->text("img");
+            $table->text('image');
             $table->timestamps();
         });
     }
